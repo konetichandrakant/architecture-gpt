4f6fced99cf44c59b6d05f23d4d766d69e3ce3e4
konetichandrakant <konetichandrakant@gmail.com>
2026-06-10 00:19:32 -0400

    add: users schema

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -3,8 +3,24 @@ from projects import GetProjectResponse
 
 # Create
 
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    password: str
+
+class CreateUserResponse(BaseModel):
+    id: str
+    message: str
+
 # Read
 
+class GetUserRequest(BaseModel):
+    id: str
+
+class GetUserResponse(BaseModel):
+    email: str
+    username: str
+    
 # Update
 
 # Delete
